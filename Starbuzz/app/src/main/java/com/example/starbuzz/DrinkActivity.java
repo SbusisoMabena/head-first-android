b4afb264bb5cf32eff3c737a63028ed47c0e179f
Sbusiso Mabena <Sbusisomabena0@gmail.com>
2022-05-10 23:44:10 +0200

    build the drink activity

--- Starbuzz/app/src/main/java/com/example/starbuzz/DrinkActivity.java
+++ Starbuzz/app/src/main/java/com/example/starbuzz/DrinkActivity.java
@@ -2,15 +2,33 @@ package com.example.starbuzz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class DrinkActivity extends AppCompatActivity {
 
-    public static String EXTRA_DRINKID = "extra_drinkid";
+    public static String EXTRA_DRINKID = "drinkId";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drink);
+
+        Intent intent = getIntent();
+        int drinkId = (int) intent.getExtras().get(EXTRA_DRINKID);
+
+        Drink drink = Drink.drinks[drinkId];
+
+        ImageView photo = findViewById(R.id.photo);
+        photo.setImageResource(drink.getImageResourceId());
+        photo.setContentDescription(drink.getName());
+
+        TextView name = findViewById(R.id.name);
+        name.setText(drink.getName());
+
+        TextView description = findViewById(R.id.description);
+        description.setText(drink.getDescription());
     }
 }
